b4cf63fca67468c16e949873880d6ef7160c3c32
MicOng20 <danteong5210@gmail.com>
2022-03-24 21:07:26 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/utar/socialcookingapp/MainActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utar.socialcookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
