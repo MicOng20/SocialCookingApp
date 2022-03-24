748e7f791f078002eb7eb960dc45590f0cc130c0
MicOng20 <danteong5210@gmail.com>
2022-03-24 21:20:14 +0800

    First Test

--- app/src/main/java/my/edu/utar/socialcookingapp/MainActivity.java
+++ app/src/main/java/my/edu/utar/socialcookingapp/MainActivity.java
@@ -3,6 +3,7 @@ package my.edu.utar.socialcookingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.hw);
+
+        tv.setText("I am here");
+
     }
 }
