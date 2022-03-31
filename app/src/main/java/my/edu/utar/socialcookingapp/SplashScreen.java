5c93f0c81cd39188933c020f7f5e10ffe22aa5ce
xiaoqiann <xiaoqian.ngX@sophicautomation.com>
2022-03-31 11:54:58 +0800

    SplashScreen page and Connect with Google

---
+++ app/src/main/java/my/edu/utar/socialcookingapp/SplashScreen.java
@@ -0,0 +1,30 @@
+package my.edu.utar.socialcookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,Connect.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 3000);
+    }
+}
