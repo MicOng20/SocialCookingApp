67656886eb5c1197f8a375da705fd9559d2e0b8d
ljs728 <polosheng97@gmail.com>
2022-04-13 21:15:34 +0800

    TESTING

--- app/src/main/java/my/edu/utar/socialcookingapp/SearchFragment.java
+++ app/src/main/java/my/edu/utar/socialcookingapp/SearchFragment.java
@@ -61,4 +61,5 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+    //TEST
 }
